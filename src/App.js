621c38a02219881232db225cdb7eff4840d6b294
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-05-28 16:15:55 -0500

    added login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './reusables/LoginBox.js'
 
 function App() {
   return (
-    <div className="App">
-   
+    <div>
+      <Login/>
     </div>
+    
   );
 }
 
 export default App;
+
+
+
+
+// import React, { useState } from 'react';
+// import StaffLogin from './StaffLogin';
+// import ParentLogin from './ParentLogin';
+// import StudentLogin from './StudentLogin';
+
+// const LoginPage = () => {
+//   const [activeTab, setActiveTab] = useState('staff');
+
+//   const handleTabChange = (tab) => {
+//     setActiveTab(tab);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={() => handleTabChange('staff')}>Staff</button>
+//         <button onClick={() => handleTabChange('parent')}>Parent</button>
+//         <button onClick={() => handleTabChange('student')}>Student</button>
+//       </div>
+
+//       {activeTab === 'staff' && <StaffLogin />}
+//       {activeTab === 'parent' && <ParentLogin />}
+//       {activeTab === 'student' && <StudentLogin />}
+//     </div>
+//   );
+// };
+
+// export default LoginPage;
---
+++ src/reusables/LoginBox.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import '../css/Login.css';
+import BackgroundImage from "../img/grades.png"
+
+
+function LoginInBox() {
+
+    const [user, setUser] = useState({})
+
+
+    const changeHandler = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        const tempUser = { ...user }
+        tempUser[name] = value
+        setUser(tempUser)
+    }
+
+    const submitHandler = () => {
+
+
+
+        axios.post("", user)
+            .then((response) => {
+
+                console.log(response.data)
+
+
+            }).catch((e) => {
+                console.log(e.response)
+
+            })
+
+    }
+
+
+
+
+    return (
+        <header style={HeaderStyle}>
+            <div className="login-container">
+                <div className="login-box">
+                    <h2>Login</h2>
+                    <input placeholder="email" type="text" className="" onChange={changeHandler} name="email" value={user.email} />
+
+                    <input placeholder="Password" type="password" className="" onChange={changeHandler} name="password" value={user.password} />
+
+                    <button className="" onClick={submitHandler}/>
+                </div>
+            </div>
+
+
+
+
+
+
+        </header>
+    )
+}
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundColor: "white"
+}
+export default LoginInBox
